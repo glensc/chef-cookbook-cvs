5e7fa88bd7b8c6614baedafdf6f195e58646663b
Elan Ruusamäe <glen@delfi.ee>
2014-01-29 20:20:34 +0200

    version 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'glen@delfi.ee'
 license          'Apache 2.0'
 description      'Provices CVS SCM Resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
