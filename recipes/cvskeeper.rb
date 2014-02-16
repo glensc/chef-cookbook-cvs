aae74a4d9c41676131c6e42dc2497620b28eb22a
Elan Ruusamäe <glen@delfi.ee>
2014-02-16 15:23:19 +0200

    setup chef handler for current chef run as well

--- recipes/cvskeeper.rb
+++ recipes/cvskeeper.rb
@@ -9,7 +9,14 @@ end
 file '/etc/chef/client.d/cvskeeper-handler.rb' do
   content <<-EOQ
 require '#{node.chef_handler.handler_path}/cvskeeper-handler.rb'
+# For Chef < 11.10: setup default for event_handlers. CHEF-4363
 event_handlers [] unless event_handlers
 event_handlers << Cvskeeper::EventHandler.new
 EOQ
+end
+
+chef_handler 'Cvskeeper::EventHandler' do
+  source "#{node.chef_handler.handler_path}/cvskeeper-handler.rb"
+  action :enable
+  supports :event => true
 end
