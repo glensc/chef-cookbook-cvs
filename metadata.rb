af3dc67f6f4757b011a52431df083538ec259c2c
Elan Ruusamäe <glen@delfi.ee>
2017-04-19 21:31:25 +0300

    update metadata version to 0.2.3

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'cvs'
 maintainer       'Elan Ruusamäe'
-maintainer_email 'glen@delfi.ee'
+maintainer_email 'glen@pld-linux.org'
 license          'Apache 2.0'
 description      'Provices CVS SCM Resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version          '0.2.3'
 
 depends 'chef_handler'
 depends 'chef-client'
